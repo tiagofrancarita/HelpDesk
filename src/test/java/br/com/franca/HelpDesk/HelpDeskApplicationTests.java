50523e30dea44e6bac197928590ebf7ad300b940
Tiago França <tiagofranca.rita@gmail.com>
2024-05-20 00:31:41 -0300

    init projet

---
+++ src/test/java/br/com/franca/HelpDesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franca.HelpDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
