5731d79fa09f782a9a2cfd442621cdd091fbdde7
Tiago França <tiagofranca.rita@gmail.com>
2024-05-21 00:05:23 -0300

    Ajuste em anotações das classes, e execução do projeto

--- src/main/java/br/com/franca/HelpDesk/domains/Cliente.java
+++ src/main/java/br/com/franca/HelpDesk/domains/Cliente.java
@@ -1,8 +1,10 @@
 package br.com.franca.HelpDesk.domains;
 
+import br.com.franca.HelpDesk.domains.enums.PerfilEnum;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import java.util.ArrayList;
@@ -14,13 +16,16 @@ import java.util.List;
 @Data
 public class Cliente extends Pessoa {
 
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
         super();
+        addPerfil(PerfilEnum.CLIENTE);
     }
 
     public Cliente(Long id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(PerfilEnum.CLIENTE);
     }
 }
